1adbf7a1081822b301ce54fa9a88c5a812aa0f52
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-03-07 18:58:33 +0100

    Added a C example of a delegate that returns void

---
+++ examples/c/PrintDelegateVoid.h
@@ -0,0 +1,11 @@
+#ifndef EXAMPLE_PRINT_DELEGATE_VOID_H
+#define EXAMPLE_PRINT_DELEGATE_VOID_H
+
+#define RETURN_TYPE void
+#define RETURN_TYPE_IS_VOID 1
+#define ARGUMENTS int arg1
+#define ARGUMENTS_NAMES arg1
+#define NAME PrintDelegateVoid
+#include "../../include/delegate.h"
+
+#endif
--- examples/c/delegates.c
+++ examples/c/delegates.c
@@ -3,15 +3,29 @@
 
 #define DELEGATE_IMPLEMENTATION
 #include "PrintDelegate.h"
+#define DELEGATE_IMPLEMENTATION
+#include "PrintDelegateVoid.h"
 
 int PrintNumber(int x, int y)
 {
 	printf("x = %d, y = %d\n", x,y);
+	return 1;
 }
 
 int PrintMoney(int x, int y)
 {
 	printf("DINERO! x = %d, y = %d\n", x*2,y*3);
+	return 2;
+}
+
+void PrintNumberVoid(int x)
+{
+	printf("Number: %d\n", x);
+}
+
+void PrintMoneyVoid(int x)
+{
+	printf("Money: %d\n", x);
 }
 
 int main()
@@ -23,8 +37,17 @@ int main()
 	PrintDelegate_add(&delegate, PrintNumber);
 	PrintDelegate_add(&delegate, PrintMoney);
 	PrintDelegate_remove(&delegate, PrintMoney);
-	PrintDelegate_invoke(&delegate,10,20);
-	//PrintDelegate_invoke(&delegate,30,40);
+	int x = PrintDelegate_invoke(&delegate,10,20);
 	PrintDelegate_free(&delegate);
+	printf("x = %d\n", x);
+	
+	
+	PrintDelegateVoid delegate2;
+	PrintDelegateVoid_init(&delegate2);
+	PrintDelegateVoid_add(&delegate2, PrintNumberVoid);
+	PrintDelegateVoid_add(&delegate2, PrintMoneyVoid);
+	PrintDelegateVoid_invoke(&delegate2, 69);
+	PrintDelegateVoid_free(&delegate2);
+	
 	return 0;
 }
